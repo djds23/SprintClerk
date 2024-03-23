694285dbdfe259632707dd3cb9e058096d4e86f5
Dean Silfen <dean.silfen@gmail.com>
2024-03-23 15:37:22 -0400

    Initial Commit

---
+++ SprintClerk/SprintClerkApp.swift
@@ -0,0 +1,32 @@
+//
+//  SprintClerkApp.swift
+//  SprintClerk
+//
+//  Created by Dean Silfen on 3/23/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SprintClerkApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
